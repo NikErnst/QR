fcebc1a14ae55a014d17f5c5a66bd0c4973a8e34
Ernst Nik <ernstn@mail.ru>
2022-11-05 20:07:31 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper"),
+const wrapper = wrapper.querySelector(".form button");
+
+generateBtn.addEventListener("click", () => {
+	wrapper.classList.add("active");
+});
